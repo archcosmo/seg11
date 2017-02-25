07e071fdc6508b080b97a4cdd650f2f946150dcb
Nick Ktori <Nick@Nicks-MacBook-Pro.local>
2017-02-25 17:45:45 +0000

    Added Threshold object

---
+++ core/Threshold.java
@@ -0,0 +1,10 @@
+public class Threshold {
+	int tora, toda, asda, lda;
+	
+	public Threshold(int tora, int toda, int asda, int lda) {
+		this.tora = tora;
+		this.toda = toda;
+		this.asda = asda;
+		this.lda = lda;
+	}	
+}
