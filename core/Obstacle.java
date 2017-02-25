07a25b5a7ecc0e3cc75232fd16f1852b77bee9f7
Nick Ktori <Nick@Nicks-MacBook-Pro.local>
2017-02-25 17:44:30 +0000

    Added Obstacle object

---
+++ core/Obstacle.java
@@ -0,0 +1,13 @@
+public class Obstacle {
+	int height;
+	int width;
+	int length;
+	String name;
+	
+	public Obstacle(int height, int width, int length, String name) {
+		this.height = height;
+		this.width = width;
+		this.length = length;
+		this.name = name;
+	}
+}
