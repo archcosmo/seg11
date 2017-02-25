24cac04f10b02138f026f01a25d123052494a408
Validifyed <Validifyed@users.noreply.github.com>
2017-02-25 18:43:40 +0000

    Update Console.java

--- core/Console.java
+++ core/Console.java
@@ -3,7 +3,7 @@ package core;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-public class Console implements Updatable
+public class Console 
 {
 	Scanner s;
 	Controller controller;
@@ -32,14 +32,22 @@ public class Console implements Updatable
 	/* Gets user input */
 	private String[] getInput()
 	{
-		System.out.print("input: ");
+		System.out.print("$ : ");
 		
 		s = new Scanner(System.in);
 		String input = s.nextLine();
 		return input.split(" ");
 	}
 	
+	public void printList(String[] list)
+	{
+		
+	}
+	
 	/* Public input resolver
+	 * resolves first argument
+	 * checks number of arguments based on function
+	 * resolves further arguments
 	 * returns quit_flag
 	 */
 	public boolean handle_input()
@@ -48,17 +56,85 @@ public class Console implements Updatable
 		String[] input = getInput();
 		switch( input[0] ) 
 		{
-			case "help":
+		case "help":
+			if ( input.length == 1 ) 
+			{
 				System.out.println("Placeholder Help Page..."); /* PLACEHOLDER HELP DOCUMENTATION */
-				break;
-			case "quit":
+			} else { wrong_args(input); }
+			break;
+		case "quit":
+			if ( input.length == 1 ) 
+			{
 				quit_flag = true;
-				break;
-			default:
-				System.out.println("Invalid input. Use 'help' for a list of commands and uses.");
-				break;
+			} else { wrong_args(input); }
+			break;
+		case "list":
+			if ( input.length == 2 ) 
+			{
+				switch ( input[1] )
+				{
+				case "airports":
+					controller.getAirports();
+					break;
+				case "runways":
+					controller.getRunways();
+					break;
+				case "objects":
+					controller.getObjects();
+					break;
+				default:
+					System.out.println("Invalid argument to command 'list (type)'\n : Valid types are; 'airports', 'runways', 'objects'");
+					break;
+				}
+			} else { wrong_args(input); }
+			break;
+		case "add":
+			if ( input.length == 2 ) 
+			{
+				
+			} else { wrong_args(input); }
+			break;
+		case "delete":
+			if ( input.length == 3 ) 
+			{
+				switch ( input[1] )
+				{
+				case "airport":
+					controller.getAirports();
+					break;
+				case "runway":
+					controller.getRunways();
+					break;
+				case "object":
+					controller.getObjects();
+					break;
+				default:
+					System.out.println("Invalid argument to command 'delete (type) (id)'\n : Valid types are; 'airport', 'runway', 'object'");
+					break;
+				}
+			} else { wrong_args(input); }
+			break;
+		case "calculate":
+			if ( input.length == 1 ) 
+			{
+				// Recalculate and show
+			}
+			else if (input[1] == "-n" )
+			{
+				// Show calculations no recalculate
+			} else { wrong_args(input); }
+			break;
+		default:
+			for ( int i = 0; i < input.length; i++) System.out.print(input[i] + " ");
+			System.out.println(" : Command not found");
 		}
 		return quit_flag;
+	}
+	
+	private void wrong_args(String[] input)
+	{
+		for ( int i = 0; i < input.length; i++) System.out.print(input[i] + " ");
+		System.out.println(" : Invalid number of arguments");
 	}
 	
 	/* Displays quit sign 
@@ -69,10 +145,5 @@ public class Console implements Updatable
 		printBar("Exiting Application");
 		try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); } // Sleep 3 seconds
 		s.close();
-	}
-
-	@Override
-	public void updateView(String msg) {
-		System.out.println(msg);
 	}
 }
