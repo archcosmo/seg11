eec76b694a3f9c85cfee8308442aea213c100489
Arch Menzies <am3g15@soton.ac.uk>
2017-02-25 17:26:35 +0000

    Added Updatadable interface :wq

---
+++ core/Updatable.java
@@ -0,0 +1,6 @@
+package core;
+
+public interface Updatable {
+	//TODO: Change to pass structured data when GUI is implemented
+	public void updateView(String msg); //Called by model to notify view of changes.
+}
