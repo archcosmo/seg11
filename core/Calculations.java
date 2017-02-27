f3f438bcd2a8f2cee475b5f9782e385b9b7e01c7
Michael P-F <mikeyredtiger22@gmail.com>
2017-02-27 19:40:03 +0000

    Added take-off / land after obstacle calculations

--- core/Calculations.java
+++ core/Calculations.java
@@ -2,53 +2,86 @@ package core;
 
 public class Calculations {
 
-    private String lastCalculationBreakdown;
+    String lastCalculationBreakdown;
+    int toda, tora, asda, lda, stopwayLength, stopwayWidth, clearway;
 
-    public void calculateDistances(int Threshold, int obstacle, int xPos, int yPos){
-        //takes int, calculates and returns real number.
-    	
-    	calculateTORA();
-    	calculateTODA();
-    	calculateASDA();
-    	calculateLDA();
+    //TODO: if obstacle lies on stopway/clearway
+    //TODO: use hight of obstacle to calculate take-off
+
+    public Threshold calculateDistances(Threshold threshold, int stopwayLength, int stopwayWidth, int clearway,
+                                        Obstacle obstacle, int obstacleXPos, int obstacleYPos) {
+        /*
+        takes int, calculates and returns real number.
+        if obstacle closer to start of runway, calculate landing over and taking off after obstacle
+        assuming obstaclePos is the position of the middle of the obstacle
+        */
+
+        int newTora;
+        int newToda;
+        int newAsda;
+        int newLda;
+
+        //Calculate which half of runway, the obstacle is on
+        if (obstacleXPos > (threshold.tora / 2.0)) {
+            //Take off / land before obstacle
+            newTora = threshold.tora;
+            newToda = threshold.toda;
+            newAsda = threshold.asda;
+            newLda = threshold.tora;
+        } else {
+            //Take off / land after obstacle
+            //TODO: engine blast radius
+            newTora = threshold.tora - obstacleXPos;
+            newToda = threshold.toda - obstacleXPos + clearway;
+            newAsda = threshold.asda - obstacleXPos + stopwayWidth;
+            //TODO: displaced thresholds
+            newLda = threshold.tora - (240 + 60); //new RESA + strip-end
+        }
+        /*
+        calculateTORA();
+        calculateTODA();
+        calculateASDA();
+        calculateLDA();
+        */
+        return new Threshold(threshold.designator, newTora, newToda, newAsda, newLda);
     }
-    
+
     public String getLastCalculationBreakdown() {
         return lastCalculationBreakdown;
     }
-    
-    int toda, tora, asda, lda, stopwayLength, clearway;
-    
+
+
     /*
      * All calculations need functionality for either direction and
      * how to choose which calculation to use. 
      * Other values need to also be created/stored (not sure how this is handled by console)
-     */
+     *
     public int calculateTORA() {
-    	
+
     }
-    
+
     /*
      * TODA and ASDA calculations use recalculated TORA value
-     */
+     *
     public int calculateTODA() {
-    	if (towards) return tora;
-    	else 
-    		return tora + clearway;
+        if (towards) return tora;
+        else
+            return tora + clearway;
     }
-    
+
     public int calculateASDA() {
-    	if (towards) return tora;
-    	else
-    		return tora + stopwayLength;
-    } 
-    
-    public int calculateLDA() {
-    	if (towards) 
-    		return (distance from threshold) - (RESA) - (strip end);
-    	else 
-    		return lda - (obstacleHeight*50) - (distance from threshold) - (strip end); 
+        if (towards) return tora;
+        else
+            return tora + stopwayLength;
     }
+
+    public int calculateLDA() {
+        if (towards)
+            return (distance from threshold)-(RESA) - (strip end);
+        else
+        return lda - (obstacleHeight * 50) - (distance from threshold)-(strip end);
+    }
+    */
 }
 
 /*
