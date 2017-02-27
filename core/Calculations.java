8a4c60f4538346d0386c179c315567155d7273c8
mikeyredtiger22 <mikeyredtiger22@gmail.com>
2017-02-27 13:26:33 +0000

    Added Calculations file
    Skeletal Class.

---
+++ core/Calculations.java
@@ -0,0 +1,12 @@
+public class Calculations {
+
+    private String lastCalculationBreakdown;
+
+    public void calculateDistances(int Threshold, int obstaacle, int xPos, int yPos){
+        //takes int, calculates and returns real number.
+    }
+    public String getLastCalculationBreakdown() {
+        return lastCalculationBreakdown;
+    }
+
+}
