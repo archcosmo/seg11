e0c9a6ad8587b63e330a12945c75a23cff73a426
Michael P-F <mikeyredtiger22@gmail.com>
2017-02-27 23:31:37 +0000

    Changed Obstacle class and usage

--- core/Obstacle.java
+++ core/Obstacle.java
@@ -1,15 +1,16 @@
 package core;
 
 public class Obstacle {
+
+	int left; //Distance from start of runway to start of obstacle
+	int right; //Distance from start of runway to end of obstacle
 	int height;
-	int width;
-	int length;
 	String name;
 	
-	public Obstacle(int height, int width, int length, String name) {
+	public Obstacle(int left, int right, int height, String name) {
+		this.left = left;
+		this.right = right;
 		this.height = height;
-		this.width = width;
-		this.length = length;
 		this.name = name;
 	}
 }
