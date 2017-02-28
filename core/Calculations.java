1498b292eece9da872f8b2bcff4ed29e02eef67e
Michael P-F <mikeyredtiger22@gmail.com>
2017-02-28 23:59:05 +0000

    Calculations return thresholds arraylist (for now)

--- core/Calculations.java
+++ core/Calculations.java
@@ -1,45 +1,59 @@
 package core;
 
+import java.util.ArrayList;
+
 public class Calculations {
 
     private static final int DEFAULT_RESA = 240;
     private static final int DEFAULT_ANGLE_OF_DESCENT = 50;
     private static final int DEFAULT_BLAST_ALLOWANCE = 300;
 
-    String lastCalculationBreakdown;
+    private String lastCalculationBreakdown;
 
     //TODO: can take whole runway object (and calculate for each logical runway)
-    public void calculateDistances(LogicalRunway runway, Obstacle obstacle, int blastAllowance /* = -1 */) {
+    public ArrayList<Integer> calculateDistances(LogicalRunway logicalRunway, Obstacle obstacle, int blastAllowance /* = -1 */) {
         int newTora;
         int newToda;
         int newAsda;
         int newLda;
 
         //TODO: if obstacle not inside cleared and graded area, no need to redeclare distances
+		//if (obstacle.bottomYPos > topOfRunway || obstacle.topYPos < bottomOfRunway) {
+		//	return logicalRunway.getThresholdClass
+		//}
 
+
+		//TODO: displaced thresholds
+		//TODO: obstacle distance: from which end of runway?
 		double obstacleMidXPosition = obstacle.left + ((obstacle.right - obstacle.left) / 2.0);
         //Calculate which half of runway, the obstacle is on
-        if (obstacleMidXPosition > (runway.tora / 2.0)) {
+        if (obstacleMidXPosition > (logicalRunway.tora / 2.0)) {
             //Take off / land before obstacle
             int ALSWidth = DEFAULT_ANGLE_OF_DESCENT * obstacle.height;
-            int ALSDistance = runway.tora - obstacle.left - ALSWidth;
-            newTora = runway.tora - runway.stopwayLength - Math.max(DEFAULT_RESA, ALSDistance);
+            int ALSDistance = logicalRunway.tora - obstacle.left - ALSWidth;
+            newTora = logicalRunway.tora - logicalRunway.stopwayLength - Math.max(DEFAULT_RESA, ALSDistance);
             newToda = newTora;
             newAsda = newTora;
-            newLda = obstacle.left -(DEFAULT_RESA + runway.stopwayLength);
+            newLda = obstacle.left -(DEFAULT_RESA + logicalRunway.stopwayLength);
         } else {
             //Take off / land after obstacle
             if (blastAllowance == -1) {
                 blastAllowance = DEFAULT_BLAST_ALLOWANCE;
             }
-            newTora = runway.tora - obstacle.left;
-            newToda = runway.toda - obstacle.left - runway.clearwayLength;
-            newAsda = runway.asda - obstacle.left - runway.stopwayLength;
-            //TODO: displaced thresholds
+            newTora = logicalRunway.tora - obstacle.left;
+            newToda = logicalRunway.toda - obstacle.left - logicalRunway.clearwayLength;
+            newAsda = logicalRunway.asda - obstacle.left - logicalRunway.stopwayLength;
             int ALSWidth = DEFAULT_ANGLE_OF_DESCENT * obstacle.height;
-            newLda = runway.tora - obstacle.left - Math.max(blastAllowance, runway.stopwayLength + Math.max(DEFAULT_RESA, ALSWidth));
+            newLda = logicalRunway.tora - obstacle.left - Math.max(blastAllowance, logicalRunway.stopwayLength + Math.max(DEFAULT_RESA, ALSWidth));
         }
         //TODO: return new Threshold Class
+        ArrayList<Integer> thresholds = new ArrayList<>();
+        thresholds.add(newTora);
+        thresholds.add(newToda);
+        thresholds.add(newAsda);
+        thresholds.add(newLda);
+
+        return thresholds;
     }
 
     public String getLastCalculationBreakdown() {
