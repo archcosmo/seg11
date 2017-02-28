64109dc3ea38df85e6b9c3b42ab669e91d2302bc
Michael P-F <mikeyredtiger22@gmail.com>
2017-02-28 23:58:17 +0000

    Calculations return threshold arraylist (for now)

--- tests/CalculationsTest.java
+++ tests/CalculationsTest.java
@@ -5,21 +5,24 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class CalculationsTest {
 	Calculations calc = new Calculations();
 
 	@Test
 	public void testTORA() {
-		LogicalRunway nineR = new LogicalRunway("09R", 3660, 3660, 3660, 3353);
-		LogicalRunway twentySevenL = new LogicalRunway("27L", 3660, 3660 ,3660, 3660);
-		LogicalRunway nineL = new LogicalRunway("09L", 3902, 3902, 3902, 3595);
-		LogicalRunway twentySevenR = new LogicalRunway("27R", 3884, 3962, 3884 , 3884);
+		Runway testRunway = new Runway("Test Runway", 240, 300); //Using default values - changeable!
+		LogicalRunway nineR = new LogicalRunway("09R", testRunway, 3660, 3660, 3660, 3353);
+		LogicalRunway twentySevenL = new LogicalRunway("27L", testRunway, 3660, 3660 ,3660, 3660);
+		LogicalRunway nineL = new LogicalRunway("09L", testRunway, 3902, 3902, 3902, 3595);
+		LogicalRunway twentySevenR = new LogicalRunway("27R", testRunway, 3884, 3962, 3884 , 3884);
 		
-		Obstacle obstacle = new Obstacle(50,50,12,"Object");
+		Obstacle obstacle = new Obstacle("Test Object", 50 ,50,12, -10, 10);
 		
-		LogicalRunway newRun = calc.calculateDistances(nineL, obstacle, 300);
-		System.out.println(newRun.tora);
-		assertEquals("09L Away/Over = 3346", 3346, newRun.tora);
+		ArrayList<Integer> thresholds = calc.calculateDistances(nineL, obstacle, 300);
+		System.out.println(thresholds.get(1));
+		assertEquals("09L Away/Over = 3346", 3346, thresholds.get(1));
 //		assertEquals("27R Towards = 2986", 2986, );
 //		assertEquals("09R Towards = 1850", 1850, );
 //		assertEquals("27L Away/Over = 2860", 2860, );
