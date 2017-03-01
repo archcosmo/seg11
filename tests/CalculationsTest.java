a2f7c67fe420bf22f0f028f183ee6852f00d0d50
npk1g15 <npk1g15@soton.ac.uk>
2017-03-01 10:51:43 +0000

    Changed test layout to test each scenario seperately

--- tests/CalculationsTest.java
+++ tests/CalculationsTest.java
@@ -9,26 +9,41 @@ import java.util.ArrayList;
 
 public class CalculationsTest {
 	Calculations calc = new Calculations();
+	Runway testRunway = new Runway("Test Runway", 240, 300); //Using default values - changeable!
+	LogicalRunway nineR = new LogicalRunway("09R", testRunway, 3660, 3660, 3660, 3353);
+	LogicalRunway twentySevenL = new LogicalRunway("27L", testRunway, 3660, 3660 ,3660, 3660);
+	LogicalRunway nineL = new LogicalRunway("09L", testRunway, 3902, 3902, 3902, 3595);
+	LogicalRunway twentySevenR = new LogicalRunway("27R", testRunway, 3884, 3962, 3884 , 3884);
 
 	@Test
-	public void testTORA() {
-		Runway testRunway = new Runway("Test Runway", 240, 300); //Using default values - changeable!
-		LogicalRunway nineR = new LogicalRunway("09R", testRunway, 3660, 3660, 3660, 3353);
-		LogicalRunway twentySevenL = new LogicalRunway("27L", testRunway, 3660, 3660 ,3660, 3660);
-		LogicalRunway nineL = new LogicalRunway("09L", testRunway, 3902, 3902, 3902, 3595);
-		LogicalRunway twentySevenR = new LogicalRunway("27R", testRunway, 3884, 3962, 3884 , 3884);
-		
-		Obstacle obstacle = new Obstacle("Test Object", 50 ,50,12, -10, 10);
+	public void testScenario1AwayOver() {
+		Obstacle obstacle = new Obstacle("Test Object", -50, -50, 12, -10, 10);
 		
 		ArrayList<Integer> thresholds = calc.calculateDistances(nineL, obstacle, 300);
-		System.out.println(thresholds.get(1));
-		assertEquals("09L Away/Over = 3346", 3346, thresholds.get(1));
-//		assertEquals("27R Towards = 2986", 2986, );
-//		assertEquals("09R Towards = 1850", 1850, );
-//		assertEquals("27L Away/Over = 2860", 2860, );
-//		assertEquals("09R Away/Over = 2903", 2903, );
-//		assertEquals("27L Towards = 2393", 2393, );
-//		assertEquals("09L Towards = 2792", 2792, );
-//		assertEquals("27R Away/Over = 3534", 3534, );
+		System.out.println("TORA (3346): " + thresholds.get(0));
+		System.out.println("TODA (3346): " + thresholds.get(1));
+		System.out.println("ASDA (3346): " + thresholds.get(2));
+		System.out.println("LDA (2985): " + thresholds.get(3));
+		assertEquals("09L Away/Over TORA", 3346, (int) thresholds.get(0));
+		assertEquals("09L Away/Over TODA", 3346, (int) thresholds.get(1));
+		assertEquals("09L Away/Over ASDA", 3346, (int) thresholds.get(2));
+		assertEquals("09L Away/Over LDA", 2985, (int) thresholds.get(3));
+		
 	}
+//	
+//	@Test
+//	public void testScenario1Toward() {
+//		Obstacle obstacle = new Obstacle("Test Object", 50 ,50,12, -10, 10);
+//		
+//		ArrayList<Integer> thresholds = calc.calculateDistances(nineL, obstacle, 300);
+//		System.out.println("TORA (2986): " + thresholds.get(0));
+//		System.out.println("TODA (2986): " + thresholds.get(1));
+//		System.out.println("ASDA (2986): " + thresholds.get(2));
+//		System.out.println("LDA (3346): " + thresholds.get(3));
+//		assertEquals("09L Away/Over TORA", 2984, (int) thresholds.get(0));
+//		assertEquals("09L Away/Over TODA", 2984, (int) thresholds.get(1));
+//		assertEquals("09L Away/Over ASDA", 2984, (int) thresholds.get(2));
+//		assertEquals("09L Away/Over LDA", 3346, (int) thresholds.get(3));
+//		
+//	}
 }
