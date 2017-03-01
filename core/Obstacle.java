618dd60f15e772412463f858ef1dbc2d275ffbe4
Michael P-F <mikeyredtiger22@gmail.com>
2017-03-01 23:27:00 +0000

    CODE CLEANUP (read commit description)
    some indent changes
    moved class member variables to top of class
    initialise class member variables in constructor
    Calculations dont need blast allowance parameter because this is given
    by the runway
    Direction of runway/aircraft is given by user selecting the Logical
    runway (only 1 direction)
    Aircraft landing before/after obstacle is determined by obstacle
    placement, not the Logical runway designator
    Calculations and breakdown layout neatened
    Runway RESA and blast allowance used instead of default values.
    Removed redundant blast allowance and direction from every calculations
    test

--- core/Obstacle.java
+++ core/Obstacle.java
@@ -3,23 +3,25 @@ package core;
 public class Obstacle {
 
 	String name;
-	int xPos = 0; //Distance from start of runway to start of obstacle
-	int yPos = 0; //Distance from runway centre line to top of object in birds eye view, can be negative
-	int width, length, height;
+	int xPos; //Distance from start of runway to start of obstacle
+	int yPos; //Distance from runway centre line to top of object in birds eye view, can be negative
+	int width, length, height; //Length is along runway, width is only viewable from birs eye view
+	int fromC, fromL, fromR;
 	
 	public Obstacle(String name, int width, int length, int height) {
 		this.name = name;
 		this.width = width;
 		this.length = length;
 		this.height = height;
+		this.xPos = 0;
+		this.yPos = 0;
 	}
 	
 	public void setPos(int xPos, int yPos) {
 		this.xPos = xPos;
 		this.yPos = yPos;
 	}
-	
-	int fromC, fromL, fromR;
+
 	public void setFrom(int c, int l, int r) {
 		this.fromC = c;
 		this.fromL = l;
--- core/Runway.java
+++ core/Runway.java
@@ -1,14 +1,13 @@
 	package core;
 
 	import java.util.ArrayList;
-import java.util.List;
+	import java.util.List;
 
 	public class Runway {
 		String name;
 		List<LogicalRunway> logicalRunways;
 		int RESA, blastAllowance, stripEnd;
 
-		//TODO: where should runway RESA and blast allowance be stored?
 		public Runway(String name, int RESA, int blastAllowance, int stripEnd) {
 			this.name = name;
 			this.RESA = RESA;
