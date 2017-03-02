bad5b76c4c26a65f709fbf7a01277221614f9acc
Michael P-F <mikeyredtiger22@gmail.com>
2017-03-02 15:08:53 +0000

    Fixed cleared and graded calculation

--- core/Calculations.java
+++ core/Calculations.java
@@ -22,8 +22,7 @@ public class Calculations {
 		int newAsda;
 		int newLda;
 
-		if (Math.abs(obstacle.distanceFromThreshold) < 75 &&
-			Math.abs(obstacle.distanceFromThreshold) < 60 ) {
+		if (Math.abs(obstacle.distanceFromThreshold) > 75 && (obstacle.distanceFromThreshold > 60 || obstacle.distanceFromThreshold > logicalRunway.tora + 60)) {
 			ArrayList<Integer> thresholds = new ArrayList<>();
 			thresholds.add(logicalRunway.tora);
 			thresholds.add(logicalRunway.toda);
