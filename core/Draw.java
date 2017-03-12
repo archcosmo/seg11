3d5058892261870d1c3794a3b24ac71399e75b1a
Arch Menzies <am3g15@soton.ac.uk>
2017-03-12 20:51:46 +0000

    Draw class code update

--- core/Draw.java
+++ core/Draw.java
@@ -2,6 +2,7 @@ package core;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.*;
 
 public class Draw 
@@ -15,13 +16,21 @@ public class Draw
 	}
 
 	public void drawTopView(Graphics2D g2d, int width, int height) {
+		/*Initial rotation to 0 so runway doesn't become unaligned*/
+		AffineTransform at = new AffineTransform();
+		g2d.setTransform(at);
+		
 //		int runwayLength = model.selectedRunway.runwayLength;
 //		int runwayWidth = model.selectedRunway.runwayWidth;
+		
+		Runway runway = new Runway(300, 240, 100);
+		runway.setLogicalRunways(new LogicalRunway("09", runway, 3000, 3000, 3000, 2800, 400), new LogicalRunway("27", runway, 3000, 3000, 3000, 2800, 200));
+		
 		
 		int runwayLength = 3000;
 		int runwayWidth = 200;
 		
-		float scale = 0.8F * width / runwayLength;
+		float scale = 0.8F * width / (runwayLength + runway.shortAngleLogicalRunway.stopwayLength + runway.longAngleLogicalRunway.stopwayLength);
 		
 		int adjustedRunwayLength = (int)(scale * runwayLength);
 		int adjustedRunwayWidth = (int)(scale * runwayWidth);
@@ -29,9 +38,9 @@ public class Draw
 		int runwayX = width/2 - adjustedRunwayLength/2; 
 		int runwayY = height/2 - adjustedRunwayWidth/2;
 		
-		g2d.setColor(Color.gray);
-		g2d.fillRect(runwayX, runwayY, adjustedRunwayLength, adjustedRunwayWidth);
-
+		
+		drawRunwayTop(g2d, runway, runwayX, adjustedRunwayLength, adjustedRunwayWidth, height/2, scale);
+		
 		Obstacle ob = model.selectedObstacle;
 		/* Test obstacle */ ob = new Obstacle("Plane", 100, 500, 10); ob.setPosition(760, 20);
 		if (ob != null) {
@@ -44,8 +53,8 @@ public class Draw
 		if (logRun != null) {
 			g2d.setColor(Color.LIGHT_GRAY);
 			int adjustedStopwayLength = (int) (scale * logRun.stopwayLength);
-			g2d.fillRect(runwayX - adjustedStopwayLength, runwayY, adjustedStopwayLength, adjustedRunwayWidth);
-			g2d.fillRect(runwayX+adjustedRunwayLength, runwayY, adjustedStopwayLength, adjustedRunwayWidth);
+//			g2d.fillRect(runwayX - adjustedStopwayLength, runwayY, adjustedStopwayLength, adjustedRunwayWidth);
+//			g2d.fillRect(runwayX+adjustedRunwayLength, runwayY, adjustedStopwayLength, adjustedRunwayWidth);
 			
 			//TODO: Fix positioning of arrows
 			drawMeasurement(g2d, scale, logRun.tora, width /2, 600, 90, "TORA", 600 - (height/2 +adjustedRunwayWidth/2), 600 - (height/2 +adjustedRunwayWidth/2));
@@ -53,6 +62,74 @@ public class Draw
 			drawMeasurement(g2d, scale, logRun.asda, width /2, 450, 90, "ASDA", 10, 10);
 			drawMeasurement(g2d, scale, logRun.lda, width /2, 475, 90, "LDA", 10, 10);
 		}
+	}
+	
+	private void drawRunwayTop(Graphics2D g2d, Runway runway, int runwayX, int runwayLength, int runwayWidth, int centerlineY, float scale) {
+		
+		g2d.setColor(Color.gray);
+		g2d.fillRect(runwayX, centerlineY - runwayWidth/2, runwayLength, runwayWidth);
+		
+		/*Draw Runway designators*/
+		String lowDesig = runway.shortAngleLogicalRunway.designator;
+		String highDesig = runway.longAngleLogicalRunway.designator;
+		
+		g2d.setColor(Color.WHITE);
+		
+		AffineTransform at = new AffineTransform();
+		
+		Font gFont = g2d.getFont();
+		/*Scale designator font*/
+		g2d.setFont(new Font(gFont.getFontName(), gFont.getStyle(), (int)(80 * scale)));
+		
+		int lowDesigWidth = g2d.getFontMetrics().stringWidth(lowDesig);
+		int highDesigWidth = g2d.getFontMetrics().stringWidth(highDesig);
+		
+		//Rotate text 90 deg
+		at.setToRotation(90 * Math.PI/180, (int)(runwayX + runwayLength * 0.05), centerlineY - lowDesigWidth/2);
+		g2d.setTransform(at);
+		g2d.drawChars(lowDesig.toCharArray(), 0, lowDesig.length(), (int)(runwayX + runwayLength * 0.05), centerlineY - lowDesigWidth/2);
+		
+		//Rotate text -90 deg
+		at.setToRotation(-90 * Math.PI/180, (int)(runwayX + runwayLength * 0.95), centerlineY + highDesigWidth/2);
+		g2d.setTransform(at);
+		g2d.drawChars(highDesig.toCharArray(), 0, highDesig.length(), (int)(runwayX + runwayLength * 0.95), centerlineY + highDesigWidth/2);
+		
+		//Reset rotation
+		at.setToRotation(0);
+		g2d.setTransform(at);
+		
+		/*Draw Stopways*/
+		g2d.setColor(Color.LIGHT_GRAY);
+		int adjustedLowStopwayLength = (int) (scale * runway.shortAngleLogicalRunway.stopwayLength);
+		int adjustedHighStopwayLength = (int) (scale * runway.longAngleLogicalRunway.stopwayLength);
+		g2d.fillRect(runwayX - adjustedHighStopwayLength, centerlineY - runwayWidth/2, adjustedHighStopwayLength, runwayWidth);
+		g2d.fillRect(runwayX+runwayLength, centerlineY - runwayWidth/2, adjustedLowStopwayLength, runwayWidth);
+		
+		/*Label Stopways*/
+		g2d.setColor(Color.BLACK);
+		String stopwayLabel = "Stopway";
+		
+		/*Low Angle Stopway*/
+		g2d.setFont(new Font(gFont.getFontName(), gFont.getStyle(), (int)(runway.shortAngleLogicalRunway.stopwayLength * 60 * scale/300)));
+		int stopwayLabelWidth = g2d.getFontMetrics().stringWidth(stopwayLabel);
+		int stopwayLabelHeight = g2d.getFontMetrics().getHeight();
+		
+		g2d.drawChars(stopwayLabel.toCharArray(), 0, stopwayLabel.length(), runwayX + runwayLength + adjustedLowStopwayLength/2 - stopwayLabelWidth/2, centerlineY + stopwayLabelHeight/2);
+		
+		/*High Angle Stopway*/
+		g2d.setFont(new Font(gFont.getFontName(), gFont.getStyle(), (int)(runway.longAngleLogicalRunway.stopwayLength * 60 * scale/300)));
+		stopwayLabelWidth = g2d.getFontMetrics().stringWidth(stopwayLabel);
+		stopwayLabelHeight = g2d.getFontMetrics().getHeight();
+		
+		g2d.drawChars(stopwayLabel.toCharArray(), 0, stopwayLabel.length(), runwayX - adjustedHighStopwayLength/2 - stopwayLabelWidth/2, centerlineY + stopwayLabelHeight/2);
+		
+		
+		//Reset font
+		g2d.setFont(gFont);
+	}
+	
+	private void drawLogicalRunwayMeasurementsTop(Graphics2D g2d, LogicalRunway lr, int runwayX, int runwayLength, int centerlineY, float scale) {
+		
 	}
 	
 	private void drawObstacleTop(Graphics2D g2d, Obstacle ob, int runwayX, int centerlineY, float scale) {
@@ -67,10 +144,17 @@ public class Draw
 		g2d.setColor(Color.BLACK);
 		g2d.drawRect(obX, obY, obLength, obWidth);
 		
+		//Scale font
+		Font gFont = g2d.getFont();
+		g2d.setFont(new Font(gFont.getFontName(), gFont.getStyle(), (int)(ob.width * 60 * scale/100)));
+		
 		int stringWidth = g2d.getFontMetrics().stringWidth(ob.name);
 		int fontHeight = g2d.getFontMetrics().getHeight();
 		
 		g2d.drawChars(ob.name.toCharArray(), 0, ob.name.length(), obX + obLength/2 - stringWidth/2, obY + obWidth/2 + fontHeight/2);
+		
+		//Reset font
+		g2d.setFont(gFont);
 	}
 
 	public void drawSideView(Graphics2D g2d, int width, int height) {
