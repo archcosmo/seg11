3c9968af1ad6cc09a928112c04dbb3a8a87b5986
Michael P-F <mikeyredtiger22@gmail.com>
2017-03-15 17:43:52 +0000

    added runway width and length and runway input validation

--- core/Runway.java
+++ core/Runway.java
@@ -9,11 +9,20 @@ public class Runway {
 	LogicalRunway shortAngleLogicalRunway; //Logical runway with angle 0 to 17
 	LogicalRunway longAngleLogicalRunway;
 	int RESA, blastAllowance, stripEnd;
+	int width, length;
 
 	public Runway(int RESA, int blastAllowance, int stripEnd) {
 		this.RESA = RESA;
 		this.blastAllowance = blastAllowance;
 		this.stripEnd = stripEnd;
+	}
+
+	public Runway(int RESA, int blastAllowance, int stripEnd, int length, int width) {
+		this.RESA = RESA;
+		this.blastAllowance = blastAllowance;
+		this.stripEnd = stripEnd;
+		this.length = length;
+		this.width = width;
 	}
 
 	public void setLogicalRunways(LogicalRunway shortAngleLogicalRunway, LogicalRunway longAngleLogicalRunway) {
