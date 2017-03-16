7c7ffd90c5c712e9f5e975f8b721987f963280d7
npk1g15 <npk1g15@soton.ac.uk>
2017-03-16 15:01:08 +0000

    Fixed typo when labelling logical runways

--- core/Controller.java
+++ core/Controller.java
@@ -106,7 +106,7 @@ public class Controller
 				if (r.shortAngleLogicalRunway.designator.equals(runway.shortAngleLogicalRunway.designator)) {
 					r.shortAngleLogicalRunway.designator = r.shortAngleLogicalRunway.designator + "L";
 					r.longAngleLogicalRunway.designator = r.longAngleLogicalRunway.designator + "R";
-					r.designator = r.shortAngleLogicalRunway.designator+"/"+r.longAngleLogicalRunway;
+					r.designator = r.shortAngleLogicalRunway.designator+"/"+r.longAngleLogicalRunway.designator;
 					runway.shortAngleLogicalRunway.designator = runway.shortAngleLogicalRunway.designator + "R";
 					runway.longAngleLogicalRunway.designator = runway.longAngleLogicalRunway.designator + "L";
 					runway.designator = runway.shortAngleLogicalRunway.designator+"/"+runway.longAngleLogicalRunway.designator;
