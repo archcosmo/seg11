ddcdc11be2a96a648d7162774552c47df6e2866d
Michael P-F <mikeyredtiger22@gmail.com>
2017-04-26 18:58:22 +0100

    Validation Frame changes.

---
+++ UI/ValidateValue.java
@@ -0,0 +1,14 @@
+package UI;
+
+public class ValidateValue{
+
+	public Boolean valid;
+	public Integer value;
+	public String validationString;
+
+	public ValidateValue(Boolean valid, Integer value, String validationString) {
+		this.valid = valid;
+		this.value = value;
+		this.validationString= validationString;
+	}
+}
