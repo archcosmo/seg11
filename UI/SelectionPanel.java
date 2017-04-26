8d341a9f669a2e317487dd19ae029260458dd6ef
validifyed <validifyed@gmail.com>
2017-04-26 11:02:53 +0000

    Fixed like a million unused imports and set new classes to ignore serial id requirement

--- UI/SelectionPanel.java
+++ UI/SelectionPanel.java
@@ -1,11 +1,9 @@
 package UI;
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Insets;
 
 import javax.swing.BorderFactory;
